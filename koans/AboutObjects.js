3b98261090dc5f4fef5d94dcdb30d5e6c0dd861d
Cameron Streeter <camstreeter@gmail.com>
2018-02-11 17:38:12 -0700

    Expect current year to be 2018

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,7 +44,7 @@ describe("About Objects", function () {
       }
     };
    
-    expect(currentYear).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2018);
     expect(meglomaniac.calculateAge()).toBe(FILL_ME_IN);
   });
 
